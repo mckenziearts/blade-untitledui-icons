3cd9d298409d950d52a76cee553852166af16687
Arthur Monney <monneylobe@gmail.com>
2023-07-19 13:51:23 +0200

    Remove config file

--- src/BladeUntitledUIIconsServiceProvider.php
+++ src/BladeUntitledUIIconsServiceProvider.php
@@ -5,25 +5,18 @@ declare(strict_types=1);
 namespace Mckenziearts\BladeUntitledUIIcons;
 
 use BladeUI\Icons\Factory;
-use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
 
 final class BladeUntitledUIIconsServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->registerConfig();
-
-        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
-            $config = $container->make('config')->get('blade-untitledui-icons', []);
-
-            $factory->add('untitledui-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+        $this->callAfterResolving(Factory::class, function (Factory $factory) {
+            $factory->add('untitled-ui', [
+                'path' => __DIR__ . '/../resources/svg',
+                'prefix' => 'untitled-ui',
+            ]);
         });
-    }
-
-    private function registerConfig(): void
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-untitledui-icons.php', 'blade-untitledui-icons-config');
     }
 
     public function boot(): void
@@ -32,10 +25,6 @@ final class BladeUntitledUIIconsServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../resources/svg' => public_path('vendor/blade-untitledui-icons'),
             ], 'blade-untitledui-icons');
-
-            $this->publishes([
-                __DIR__.'/../config/blade-untitledui-icons.php' => $this->app->configPath('blade-untitledui-icons.php'),
-            ], 'blade-untitledui-icons-config');
         }
     }
 }
